644047e556479753a8313d6367a9e525caa573d2
llia464 <94816776+llia464@users.noreply.github.com>
2021-12-10 16:05:26 +1300

    logout

--- backend/src/routes/api/user.js
+++ backend/src/routes/api/user.js
@@ -19,7 +19,6 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 
-
 // const bodyParser = require('body')
 const HTTP_OK = 200; // Not really needed; this is the default if you don't set something else.
 const HTTP_CREATED = 201;
@@ -28,16 +27,6 @@ const HTTP_NO_CONTENT = 204;
 
 
 const router = express.Router();
-
-// async function fetchFromPokemonAPI() {
-//     const randomPokemonNum = Math.floor(Math.random() * 898) + 1;
-//     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomPokemonNum}`);
-//     const data = response.data;
-//     return {
-//         name: data.species.name.toUpperCase().substring(0, 1) + data.species.name.substring(1),
-//         imageUrl: data.sprites.front_default,
-//     };
-// }
 
 // Create new random pokemon
 router.post('/register', async (req, res) => {
@@ -61,32 +50,97 @@ router.post('/register', async (req, res) => {
     const existingUser = await User.findOne({ username });
     if (existingUser)
       return res.status(400).json({
-        errorMessage: "An account with this email already exists.",
+        errorMessage: "An account with this username already exists.",
       });
     
-
     // hash the password
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
-
-
 
     const newUser = {
         username: username,
         name : name,
         email : email,
         password : passwordHash};
-    // const pokemon = {
-    //     name: 'Ditto',
-    //     imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'
-    // }
-    //const pokemon = await fetchFromPokemonAPI();
+
     const dbUser = await createUser(newUser);
+
+    const token = jwt.sign(
+      {
+        user: dbUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+  // send the token in a HTTP-only cookie
+  res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    }).send();
+  
+  console.log(token);
 
     res.status(HTTP_CREATED) 
         .header('Location', `/api/user/${dbUser._id}`)
         .json(dbUser);
 });
+
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // validate
+    console.log(username);
+    console.log(password);
+
+    if (!username || !password)
+      return res
+        .status(400)
+        .json({ errorMessage: "Please enter all required fields." });
+
+    const existingUser = await User.findOne({ username });
+    if (!existingUser)
+      return res.status(401).json({ errorMessage: "Wrong email or password." });
+
+    const passwordCorrect = await bcrypt.compare(
+      password,
+      existingUser.passwordHash
+    );
+    if (!passwordCorrect)
+      return res.status(401).json({ errorMessage: "Wrong email or password." });
+
+    // sign the token
+
+    const token = jwt.sign(
+      {
+        user: existingUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    // send the token in a HTTP-only cookie
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      })
+      .send();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+router.get('/logout', (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0)
+  }).send();
+})
 
 // Retrieve all user
 router.get('/', async (req, res) => {
