77b55d5a74f7d241cf6004f2fda9b8b076026882
Ruke <rwei890@aucklanduni.ac.nz>
2022-02-04 02:51:22 +1300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,75 @@
 import {Routes, Route, useSearchParams} from 'react-router-dom';
 import ArticlesPage from './components/ArticlePage';
-import Profile from './components/Profile/Profile';
+//import Profile from './components/Profile/Profile';
 import Login from './components/Auth/signin';
 import Register from  './components/Auth/signup';
-import Editor from './components/Modify/NewMission';
 import Mission_list from './components/Modify/MissionDisplay';
-import EditMission from './components/Modify/MissionModify';
-import FactCheckerVerify from './components/Modify/FactCheckerVerify';
 import AppContext from './AppContextProvider';
-import { useContext } from 'react';
+import { useState } from 'react';
+import PageNotFound from './components/pageNotFound';
 import Article from './components/Article/article';
 import Results from './components/Results/resultsContainer';
 import Missions from './components/Mission/missionsContainer';
 import Mission from './components/Mission/missionDetail';
 import Home from './components/Home/home';
 import VerifiedArticle from './components/Results/verifiedArticle'
-import PrivateRoute from './PrivateRoute';
-import RestrictedRoute from './RestrictedRoute';
+//coco 
+//Profile
+import Profile from './components/Profile/UserProfile';
+import FactCheckerProfile from './components/Profile_FactChecker/FactChecker';
+//mission
+import Editor from './components/Modify/NewMission';
+import MyMissions from './components/Profile/MyMissions';
+import EditMission from './components/Modify/MissionModify';
+//verify
+import MissionCheck from './components/Modify/MissionCheck';
+import FactCheckerVerify from './components/Modify/FactCheckerVerify';
+import MyResults from './components/Profile_FactChecker/MyResults';
+import EditResults from './components/Modify/ResultsModify';
 
 
 export default function App() {
-  const {loggedIn} = useContext(AppContext);
-  console.log(loggedIn);
+  const {loggedIn} = useState(AppContext);
+
+  
 
   return (
+
       <Routes>
       <Route path='/' element ={<Home/>}/>
+
+      <Route path='/profile' element ={<Profile/>}/>         {/*UserProfile*/}
+      <Route path='/factCheckerProfile' element ={<FactCheckerProfile/>}/>     {/*FactCheckerProfile*/}   
+
       <Route path='/articles' element ={<ArticlesPage/>}/>
       <Route path='/articles/:id' element ={<Article/>}/>
       <Route path ='/result' element = {<Results/>} />
+      <Route path ='/result/:id' element = {<Results/>} />
       <Route path ='/result/:id/read' element = {<VerifiedArticle/>} />
       <Route path='/mission' element ={<Missions/>}/>
       <Route path='/mission/:id/read' element ={<Mission/>}/>
-      <Route path='/MissionDisplay' element ={<Mission_list/>}/>   {/* 用户创建的所有mission */}
-      <Route path='/MissionDisplay/:_id' element ={<EditMission/>}/>   {/* 编辑某用户创建的某个mission */}
-      {/* <Route path='/MissionCheck' element ={<MissionCheck/>}/>    用户创建的所有mission */}
-      <Route path='/MissionDisplay' element ={<Mission_list/>}/> 
 
-      <Route element={<PrivateRoute isLogged={loggedIn} />}>
+      <Route path='/NewMission' element ={<Editor/>}/>        {/*创建新的mission*/}
+     {/* <Route path='/MissionDisplay' element ={<Mission_list/>}/>    用户创建的所有mission */}
+      <Route path='/MyMissions' element ={<MyMissions/>}/> 
+      <Route path='/MyMissions/:_id' element ={<EditMission/>}/>   {/* 编辑某用户创建的某个mission */}
+      <Route path='/MissionCheck' element ={<MissionCheck/>}/>  {/*用户创建的所有mission */}
+      <Route path='/MissionCheck/:_id' element ={<FactCheckerVerify />}/>  {/* fact checker verify mission的界面*/}
+      <Route path='/MyResults' element ={<MyResults/>}/> 
+      <Route path='/ResultsModify/:_id' element ={<EditResults />}/>
+
+      {loggedIn ? (
+      <>
         <Route path='/profile' element ={<Profile/>}/> 
-        <Route path='/MissionCheck/:_id' element ={<FactCheckerVerify />}/>  {/* fact checker verify mission的界面*/}
-        <Route path='/NewMission' element ={<Editor/>}/>              {/*创建新的mission*/}
-      </Route>
-    
-      <Route element={<RestrictedRoute isLogged={loggedIn} />}>
-        <Route path="/signup" element ={<Register />} />
+        <Route path='/MyMissions' element ={<MyMissions/>}/> 
+      </>): (
+        <>
         <Route path="/signin" element={<Login />} />
-      </Route>
-    
+        <Route path="/signup" element ={<Register />} />
+        </>
+      )}
 
+      <Route path= "/*" element={<PageNotFound/> } />
       </Routes>
   )
 }
