10be58d3ba63ab741a14fd6daf7d585c3b0c2b82
llia464 <94816776+llia464@users.noreply.github.com>
2022-02-08 01:27:38 +1300

    mm

--- frontend/src/components/Results/resultsContainer.js
+++ frontend/src/components/Results/resultsContainer.js
@@ -1,65 +1,94 @@
-import React ,{ useState, useEffect }from 'react';
+import React ,{ useState, useEffect,useContext }from 'react';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import axios from 'axios';
 import './results.css';
-import Results from './results';
+import { Cookies } from 'react-cookie';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 import Pagination from "@mui/material/Pagination";
-import { makeStyles } from '@mui/styles';
-
-
-const useStyles = makeStyles(() => ({
-    ul: {
-      "& .MuiPaginationItem-root": {
-        color: 'rgb(233, 183, 91)',
-        padding: "20px 20px",
-        backgroundColor: '#1A2634'
-      }
-    },
-    container:{
-        justifyContent:"center",
-        alignItems:"center",
-        marginTop: "50px",
-        width:"100%"
-    },
-  }));
+import AppContext from '../../AppContextProvider';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import parse from "html-react-parser";
 
 
 export default function ResultsContainer() {
-    const [listOfResult, setListofResults] = useState([]);
-    const [filteredResult, setFilteredArticle] = useState ([]);
+    const cookies = new Cookies();
+    const username = cookies.get('username');
+    const {user, setUser, loggedIn} = useContext(AppContext); 
     const [page, setPage] = useState(1);
-    const classes = useStyles();
+    const [display, setDisplay] = useState([]);
+    const [results, setResults] = useState([]);
     const [totalPage, setTotalPage] = useState(1); 
-
-    axios.post("http://localhost:3001/api/result/resultNum").then((res)=> {
-        setTotalPage(Math.ceil(res.data/20));
-    })
-
-
+    const start = (page-1) * 20; 
+    const end = page * 20;
 
     useEffect(()=> {
-        const pageNum ={page};
-        axios.post("http://localhost:3001/api/result/resultlist", pageNum)
-       .then((response) =>{
-            setListofResults(response.data.results);
-            setTotalPage((listOfResult/20).ceil());
+        const temp = {username} 
+        axios.post("http://localhost:3001/api/user/find", temp).then((res)=> { 
+            setUser(res.data)
         })
-       .catch(()=> {console.log("ERR") } )
-   }, []);
 
-   const handleChange = (event, value) => {
-    setPage(value);
-};
+        axios.get("http://localhost:3001/api/result/resultNum").then((res)=> {
+            setTotalPage(Math.ceil(res.data/20));
+        })
 
-   const handleSearch = (e) => {
+        axios.get("http://localhost:3001/api/result/all").then((res)=> {
+            setResults(res.data);
+            setDisplay(res.data.slice(start,end));
+        }).catch(()=> {console.log("ERR") } )
+    }, []);
+
+
+    //pagination
+    useEffect(()=>{
+        const start = (page-1) * 20; 
+        const end = page * 20;
+        setDisplay(results.slice(start,end));
+    },[])
+
+
+   //set pagination
+    const handleChange = (event, value) => {
+        setPage(value);
+    };
+
+    //handle search bar 
+    const handleSearch = (e) => {
         const searchWord = e.target.value;
-        const newFilter = listOfResult.filter((value) => {
+        const newFilter = results.filter((value) => {
             return value.title.toLowerCase().includes(searchWord.toLowerCase());
         });
-        setFilteredArticle(newFilter);
-   }
+        setDisplay(newFilter);
+        if(newFilter.length == 0) {
+            setTotalPage(1);
+        }else setTotalPage(Math.ceil(newFilter.length/20));
+    }
+
+    //handle like and unlike
+    const handleLike = (id) => {
+        try{
+            const obj = {username, id};
+            if(user.arrayOfLiked.includes(id)){
+                axios.post("http://localhost:3001/api/result/unlike", obj).then((res) =>{
+                    setUser(res.data)   
+                })
+            } else{
+                axios.post("http://localhost:3001/api/result/like", obj).then((res) =>{
+                setUser(res.data)
+                })
+            }
+        }catch(err){
+            console.log(err)
+        }
+    }
+
     return (
         <div className='missions'>
             <div className='mission'>
@@ -76,13 +105,39 @@ export default function ResultsContainer() {
                 </RadioGroup>
             </div>
 
-            { filteredResult.length != 0 ? (<Results data={filteredResult}/>) :(<Results data={listOfResult}/>)}
+            <div className='resultContainer'>
+                { display.map((val, key)=> (
+                    <Card key={val._id} sx={{mb:3, p:2}}>
+                        <CardContent>
+                            <Typography gutterBottom variant="h6" component="div">
+                                {val.title}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Fact checking article Number: {val.url}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {parse(val.analysis)}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {val.author}
+                            </Typography>
+                        </CardContent>
 
-            {
-                <div className={classes.container}>
-                    <Pagination className="page" defaultPage={1} count={totalPage} page={page} onChange={handleChange} color="primary" variant="contained" classes={{ ul: classes.ul }}/>
-                </div>
-            }
+                        <CardActions>
+                            {loggedIn &&(user.arrayOfLiked.includes(val._id) ? <FavoriteIcon color="error" />: <FavoriteBorderIcon />)}
+                            { loggedIn &&  <i onClick={(e) => {handleLike(val._id)}}><ThumbUpIcon /></i> }
+                            <Button variant="text" sx={{backgroundColor: 'rgb(26,38,52)', ml: "auto"}} variant="contained" href= {`/result/${val._id}/read`} size="small"> Read more</Button>
+                        </CardActions>
+                    </Card>
+                ) ) } 
+
+            </div>
+
+            
+            <div className='pagination'>
+                <Pagination className="page" defaultPage={1} count={totalPage} page={page} onChange={handleChange} variant="outlined" />
+            </div>
+            
             
             </div>
         </div>
